aac0c3ac2bc3f7e09c3e1b553ab76ea60615a247
Renad Alosaimi <renad.alosaimi8@gmail.com>
2023-04-16 13:24:45 +0300

    create Role enum

---
+++ employeemanagment/src/main/java/com/coop/employeemanagment/infrastructures/constans/Role.java
@@ -0,0 +1,16 @@
+package com.coop.employeemanagment.infrastructures.constans;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+
+public enum Role {
+    CEO(1),
+    MANAGER(2),
+    USER(3);
+    private Long role;
+
+    Role(int role) {
+    }
+}
